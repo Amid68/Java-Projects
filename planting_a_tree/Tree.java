5ceed8c5f9c6265e4f093a13d8eb5a5b8e4c6ac3
Amid Othman <amid.othman@protonmail.com>
2024-03-03 22:18:16 +0200

    Add hello world project

---
+++ planting_a_tree/Tree.java
@@ -0,0 +1,12 @@
+public class Tree {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("I'm Amid and I'm learning to code in Java!");
+		System.out.println("I'm going to plant a tree.");
+		System.out.println("    *   ");
+		System.out.println("   ***  ");
+		System.out.println("  ***** ");
+		System.out.println("    *   ");
+		System.out.println("    *   ");
+	}
+}
